396514731a6106afaa1926b87472a80022bdaa30
Robbie Gay <robbiecgay@gmail.com>
2019-11-14 17:28:01 -0500

    Starting to work on Front End

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
 import React from 'react';
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+    axios.get(`http://127.0.0.1:8000/api/posts`)
+      .then(response => {
+        const data = response.data;
+        // this.setState({ persons });
+        console.log(data);
+      })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>
+            meow...
+            </p>
+        </header>
+      </div>
+    );
+  }
 }
+
 
 export default App;
